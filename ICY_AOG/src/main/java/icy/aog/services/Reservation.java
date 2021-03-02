a89c72f7b3b08dca62f450d0a7f2dee974edc192
pbd77 <pbd77@LAPTOP-H897RD53>
2021-03-02 15:23:54 +0900

    ok

--- ICY_AOG/src/main/java/icy/aog/services/Reservation.java
+++ ICY_AOG/src/main/java/icy/aog/services/Reservation.java
@@ -7,105 +7,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import icy.aog.beans.ReservationBean;
 
-<<<<<<< HEAD
-@Service
-public class Reservation {
-public Reservation() {
-
-}
-ModelAndView mav =new ModelAndView() ;
-
-
-public ModelAndView entrance (ReservationBean rb) {
-	ModelAndView mav=null;
-	switch (rb.getSCode()) {
-	case "LSEARCH":
-		mav= lsearchCtl(rb);
-		break;
-	case "PART":
-		mav=partCtl(rb);
-		break;
-	case "ORDER":
-		mav=orderCtl(rb);
-		break;
-	case "HDETAIL":
-		mav=hDetailCtl(rb);
-		break;
-	case "MAP":
-		mav=mapCtl(rb);
-		break;
-	case "RESFROM":
-		mav=resFromCtl(rb);
-		break;
-	case "DATECHECK":
-		mav=dateCheckCtl(rb);
-		break;
-	case "FINISH":
-		mav=finishCtl(rb);
-		break;
-	case "RESCHECK":
-		mav=resCheckCtl(rb);
-		break;
-	case "CANCLE":
-		mav=cancleCtl(rb);
-		break;
-	case "SEARCH":
-		mav=searchCtl(rb);
-		break;
-		
-	}
-	
-	return   mav;
-	
-}
-private ModelAndView searchCtl(ReservationBean rb) {
-
-	return null;
-}
-private ModelAndView cancleCtl(ReservationBean rb) {
-	
-	return null;
-}
-private ModelAndView resCheckCtl( ReservationBean rb) {
-	
-	return null;
-}
-private ModelAndView finishCtl( ReservationBean rb) {
-	
-	return null;
-}
-private ModelAndView dateCheckCtl( ReservationBean rb) {
-	
-	return null;
-}
-private ModelAndView resFromCtl( ReservationBean rb) {
-	
-	return null;
-}
-private ModelAndView mapCtl(ReservationBean rb) {
-
-	return null;
-}
-private ModelAndView hDetailCtl(ReservationBean rb) {
-	
-	return null;
-}
-private ModelAndView orderCtl(ReservationBean rb) {
-
-	return null;
-}
-private ModelAndView partCtl(ReservationBean rb) {
-	rb.setHpCode("병원코드");
-	
-	
-	mav.addObject("TEST",rb.getHpCode());
-	 mav.setViewName("part");
-	System.out.println("a11d1d");
-	return mav;
-}
-private ModelAndView lsearchCtl(ReservationBean rb) {
-=======
-
 
 @Service
 public class Reservation {
@@ -203,7 +104,7 @@ private ModelAndView partCtl( ReservationBean res) {
 	return mav;
 }
 private ModelAndView lsearchCtl(ReservationBean res) {
->>>>>>> branch 'master' of https://github.com/foxteil/ICY_AOG
+
 	
 	
 	return  null;
